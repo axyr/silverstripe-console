29c35b7a21936ac7f562f9a20648ea9b039f57f7
Martijn van Nieuwenhoven <info@axyrmedia.nl>
2016-04-17 09:51:19 +0200

    Add Publish test

---
+++ tests/InstallPublishTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Class InstallPublishTest
+ *
+ * @mixin PHPUnit_Framework_TestCase
+ */
+class InstallPublishTest extends SapphireTest
+{
+
+    /**
+     * After Composer install, the supersake file should be copied to the webroot.
+     */
+    public function testTheSuperSakeFileIsInWebRoot()
+    {
+        $this->assertTrue(is_file(BASE_PATH . '/supersake'));
+    }
+
+}
